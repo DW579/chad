c2c92ab7cdd828c5a2f7c40c69ccff120708fb6f
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2023-03-10 23:47:45 -0800

    Back end with successful calls to openai api

--- server/index.js
+++ server/index.js
@@ -1,11 +1,44 @@
 const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  async function runCompletion() {
+    try {
+      const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        temperature: 0,
+        max_tokens: 10,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+        prompt: "Explain quantum computing in simple terms",
+      });
+
+      return completion;
+    }
+    catch(error) {
+      console.log(error);
+    }
+  }
+
+  runCompletion()
+    .then((data) => {
+      res.json({message: data.data.choices[0].text})
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 });
 
 app.listen(PORT, () => {
