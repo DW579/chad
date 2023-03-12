fcba49e644b576eb1924b6a22a6d5e05308684d6
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2023-03-11 16:03:10 -0800

    GPT creating prompt for Dall E image generation

--- server/index.js
+++ server/index.js
@@ -13,16 +13,16 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-  async function runCompletion() {
+  async function chatGPTPromptCreation() {
     try {
       const completion = await openai.createCompletion({
         model: "text-davinci-003",
-        temperature: 0,
-        max_tokens: 10,
-        top_p: 1,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        prompt: "Explain quantum computing in simple terms",
+        // temperature: 0,
+        max_tokens: 100,
+        // top_p: 1,
+        // frequency_penalty: 0.0,
+        // presence_penalty: 0.0,
+        prompt: "Create me a short prompt for DALL·E 2 to create an image about a forest full of trees, and a person under a tree reading a book. This image will be used to sell books for the local book store in Redmond, wa. It should be pixel art.",
       });
 
       return completion;
@@ -32,9 +32,31 @@ app.get("/api", (req, res) => {
     }
   }
 
-  runCompletion()
+  async function dallEImageCreation(prompt) {
+    try {
+      const create_image = await openai.createImage({
+        prompt: prompt,
+        n: 1,
+        size: "1024x1024"
+      });
+
+      return create_image;
+    }
+    catch(error) {
+      console.log(error);
+    }
+  }
+
+  chatGPTPromptCreation()
     .then((data) => {
-      res.json({message: data.data.choices[0].text})
+      dallEImageCreation(data.data.choices[0].text)
+        .then((data) => {
+          console.log(data.data)
+          res.json({message: "image created"})
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     })
     .catch((error) => {
       console.log(error)
